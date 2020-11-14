6f5354def1a7262b8c89757e13c381fbdbabfb13
bdettra <bdettra8@gmail.com>
2020-11-13 16:23:35 -0800

    commit

--- ebp_auditor/settings.py
+++ ebp_auditor/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECUIRTY_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG",default=False)
